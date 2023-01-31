fd8e4be40cf3db15fb17be89387723b234e4ee3a
Mila <85253058+koseckaya@users.noreply.github.com>
2023-01-31 16:18:27 +0200

    Feature/routing (#1)
    * init: add routing
    
    * init: add routing
    
    ---------
    
    Co-authored-by: koseckaya <interika11@gmail.com>

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import './index.css';
+import { Item } from './components/Item';
+
+
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import { Main, Error, Trainings } from "./routes";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    errorElement: <Error />,
+      children: [
+      {
+        path: "trainings",
+        element: <Trainings />,
+      },
+    ],
+  },
+
+]);
+
+
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <div>Fitness App</div>
+    <Item />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
---
+++ src/routes/index.ts
@@ -0,0 +1,5 @@
+import { Error } from './Error';
+import { Main } from './Main';
+import { Trainings } from './Trainings';
+
+export { Error, Main, Trainings }
