36218cd466b9fa92dae300b4e4ca4fe81e0e9ff7
koseckaya <interika11@gmail.com>
2023-02-01 19:33:02 +0200

    feat: fix header container

--- src/routes/Header/Header.tsx
+++ src/routes/Header/Header.tsx
@@ -1,29 +1,34 @@
 
-import { FC, ComponentProps } from 'react';
+import { FC } from 'react';
 import { Link } from 'react-router-dom';
 import { Button } from '../../components/Button';
+import { Container } from '../../components/Container';
 import './Header.scss'
 
 
 export type Props = {
     className?: string;
-    testId?: string;git 
+    testId?: string;
 };
 
 const Header: FC<Props> = () => (
-    <div className= "header container" >
-         <Link to={`/`} className="logo">LOGO</Link>
-          <nav className='nav'>
-            <Link to={`/workoutPrograms`} className="nav-item">Workout Programs</Link>
-            <Link to={`/workoutVideos`} className="nav-item">Workout Videos</Link>
-            <Link to={`/recipes`} className="nav-item">Recipes</Link>
-            <Link to={`/store`} className="nav-item">Store</Link>
-            <Link to={`/about`} className="nav-item">About</Link>
-        </nav>
-        <div className='header__signUp'>
-            <Button content="Sign Up" type={Button.TYPES.DEFAULT} />
-            <Button content="Log In" type={Button.TYPES.PRIMARY} />
-        </div>
+    <div className="header" >
+        <Container>
+            <div className="header__container" >
+                <Link to={`/`} className="logo">LOGO</Link>
+                <nav className='nav'>
+                    <Link to={`/workoutPrograms`} className="nav-item">Workout Programs</Link>
+                    <Link to={`/workoutVideos`} className="nav-item">Workout Videos</Link>
+                    <Link to={`/recipes`} className="nav-item">Recipes</Link>
+                    <Link to={`/store`} className="nav-item">Store</Link>
+                    <Link to={`/about`} className="nav-item">About</Link>
+                </nav>
+                <div className='header__signUp'>
+                    <Button content="Sign Up" type={Button.TYPES.DEFAULT} />
+                    <Button content="Log In" type={Button.TYPES.PRIMARY} />
+                </div>
+            </div>
+        </Container>
     </div>
 );
 
