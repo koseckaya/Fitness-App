ea35217a1be610438e8786f132ba5c3ae2db7716
koseckaya <interika11@gmail.com>
2023-02-05 19:40:00 +0200

    feat: add filter programs & filter by duration

--- src/routes/WorkoutPrograms/WorkoutPrograms.tsx
+++ src/routes/WorkoutPrograms/WorkoutPrograms.tsx
@@ -1,3 +1,4 @@
+
 import React, { Component } from 'react'
 import { Container } from '../../components/Container'
 import { ErrorComponent } from '../../components/ErrorComponent';
@@ -5,28 +6,87 @@ import { ProgramCard } from '../../components/ProgramCard';
 import { programs } from '../../data';
 import './WorkoutPrograms.scss'
 
+const FILTER_PROGRAM = [
+  { key: 'all', value: 'All' },
+  { key: 'weightLoss', value: 'Weight Loss' },
+  { key: 'abs', value: 'Abs & Core' },
+  { key: 'booty', value: 'Booty' },
+  { key: 'fullBody', value: 'Full Body' },
+]
+
 export class WorkoutPrograms extends Component {
+
+  state = {
+    filterProgram: 'All',
+    filterDuration: null
+  };
+
+  getFilteredPrograms = () => {
+    return programs.filter(prog => {
+      if (prog.type.includes(this.state.filterProgram) || this.state.filterProgram === 'All') {
+        return true;
+      } else {
+        return false;
+      }
+    }).filter(prog => {
+      if (!this.state.filterDuration) return true;
+
+      if (this.state.filterDuration === 14 && prog.days <=28) {
+        return true
+      }
+      if (this.state.filterDuration === 28 && prog.days > 28) {
+        return true;
+      }
+      return false;
+    })
+  }
+
+  handleChangeFilter = (event: React.MouseEvent<HTMLElement>) => {
+    const target = event.target as HTMLElement
+    const prog = target.dataset.program
+    this.setState({
+      filterProgram: prog
+    });
+  }
+
+  handleChangeDuration = (event: React.ChangeEvent) => {
+    const target = event.target as HTMLSelectElement
+    const duration = +target.value
+     this.setState({
+      filterDuration: duration
+    });
+  }
+
   render() {
+    const filteredPrograms = this.getFilteredPrograms();
     return (
       <Container>
         <div className='filter'>
           <div className='filters'>
-            <span className='active'>All</span>
-            <span>Weight Loss</span>
-            <span>Abs & Core</span>
-            <span>Booty</span>
-            <span>Full Body</span>
+            {FILTER_PROGRAM.map(program => { 
+              if (this.state.filterProgram === program.value) {
+                return <span className='active'
+                  key = {program.key}
+                  data-program={program.value} onClick={this.handleChangeFilter}>{program.value}</span>
+              } else {
+                return <span data-program={program.value}
+                  key = {program.key}
+                  onClick={this.handleChangeFilter}>
+                  {program.value}</span>
+              }
+              
+            }) }
           </div>
-          <select>
-            <option>Duration</option>
-            <option>14</option>
-            <option>28</option>
+          <select onChange={this.handleChangeDuration}>
+            <option value='0'>Duration</option>
+            <option value='14'>14 - 28 days</option>
+            <option value='28'>More 28 days</option>
           </select>
         </div>
 
         <div className='programs-container'>
-          {programs.length !== 0 ? (
-              [...programs].map((i) => <ProgramCard title={i.title}
+          {filteredPrograms.length !== 0 ? (
+              filteredPrograms.map((i) => <ProgramCard title={i.title}
                 image={i.imageUrl1} url={i.path} days={i.days}
                 time={i.time} type={i.type} equip={i.equip} />
                 )
