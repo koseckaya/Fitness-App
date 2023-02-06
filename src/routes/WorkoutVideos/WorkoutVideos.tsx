3cf9a9aaef93f377bc5cbd66e8425a07d9f0b89f
koseckaya <interika11@gmail.com>
2023-02-06 18:33:09 +0200

    fead: add vodeo-prev component & finish Program

--- src/routes/WorkoutVideos/WorkoutVideos.tsx
+++ src/routes/WorkoutVideos/WorkoutVideos.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import { Container } from '../components/Container'
+import { Container } from '../../components/Container'
+import './WorkoutVideos.scss';
 
 export class WorkoutVideos extends Component {
   render() {
