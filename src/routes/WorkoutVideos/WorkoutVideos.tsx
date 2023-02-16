370bfe2730ea580fe8822bda12470ac34bf12f5e
Denis Kopych <denis.kopich@gmail.com>
2023-02-16 17:12:32 +0200

    feat: add styles for search form and filtered-videos-count

--- src/routes/WorkoutVideos/WorkoutVideos.tsx
+++ src/routes/WorkoutVideos/WorkoutVideos.tsx
@@ -10,10 +10,12 @@ interface VideoTagItem {
 }
 interface IState {
   tags: string[];
+  search: string;
 }
 export class WorkoutVideos extends Component {
   state: IState = {
     tags: ['All'],
+    search: '',
   };
 
   videoTags:VideoTagItem[] = [{
@@ -37,33 +39,83 @@ export class WorkoutVideos extends Component {
   }];
 
   handleClick = (e: React.MouseEvent<HTMLElement>) => {
+    const input = document.querySelector(".finder__input") as HTMLInputElement;
+    input.value = '';
     let clickedTag = (e.target as HTMLElement).getAttribute('data-val') || '';
     if (clickedTag === 'All'){
       this.setState({
-        tags: [clickedTag]
+        tags: [clickedTag],
+        search: '',
       });
     } else if (this.state.tags.includes(clickedTag)) {
       let neededItemIndexInState = this.state.tags.indexOf(clickedTag);
       this.setState({
-        tags: [...this.state.tags.filter((_, i) => i !== neededItemIndexInState)]
+        tags: [...this.state.tags.filter((_, i) => i !== neededItemIndexInState)],
+        search: '',
       });
       if (this.state.tags.length === 1) {
         this.setState({
-          tags: ['All']
+          tags: ['All'],
+          search: '',
         });
       }
     } else if (!this.state.tags.includes(clickedTag)) {
-      this.setState({tags: [...this.state.tags, clickedTag].filter((el) => el !== 'All')})
+      this.setState({tags: [...this.state.tags, clickedTag].filter((el) => el !== 'All'), search: '',})
     } 
   }
+  searchSubmit = (e: React.FormEvent<HTMLElement>) => {
+    const input = document.querySelector(".finder__input") as HTMLInputElement;
+    const finder = document.querySelector(".finder") as HTMLElement;
+    const valueFromInput = input.value;
+    e.preventDefault();
+    finder.classList.add("processing");
+    finder.classList.remove("active");
+    input.disabled = true;
+    setTimeout(() => {
+      finder.classList.remove("processing");
+      input.disabled = false;
+      if (input.value.length > 0) {
+        finder.classList.add("active");
+      }
+    }, 1000);
+    this.setState({tags:['All'],search: valueFromInput})
+  }
+  focusInput = (e: React.FocusEvent<HTMLInputElement>) => {
+    const finder = document.querySelector(".finder") as HTMLElement;
+    finder.classList.add("active");
+  }
   getFilteredVideos() {
-        return ([...workoutVideosData].filter(elem => [...this.state.tags].every(el => elem.categories?.includes(el))));
+        if (this.state.search === '') {
+          return ([...workoutVideosData].filter(elem => [...this.state.tags].every(el => elem.categories?.includes(el))));
+        } else {
+          return ([...workoutVideosData].filter(elem => elem.title.toLowerCase().includes(this.state.search.toLowerCase())))
+        }
   }
   render() {
     const filteredVideos = this.getFilteredVideos();
     return (
       <Container className='workout-videos'>
-        <div className='workout-videos__left-side'></div>
+        <div className='workout-videos__left-side'>
+          <div className="search-container">
+            <form autoComplete="off" onSubmit={this.searchSubmit}>
+              <div className="finder">
+                <div className="finder__outer">
+                  <div className="finder__inner">
+                    <div className="finder__icon"></div>
+                    <input className="finder__input" type="text" name="q" placeholder="Type and press Enter" onInput={this.focusInput} onBlur={() => {
+                      const input = document.querySelector(".finder__input") as HTMLInputElement;
+                      const finder = document.querySelector(".finder") as HTMLElement;
+                      if (input.value.length === 0) {
+                        finder.classList.remove("active");
+                      }
+                    }
+                    }/>
+                  </div>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
         <div className='workout-videos__right-side'>
           <div className='videos-filters'>
             {
@@ -74,6 +126,9 @@ export class WorkoutVideos extends Component {
             </span>)
             })}
           </div>
+          <div className='filtered-videos-count'>{`Showing ${
+            filteredVideos.length
+          } Workout Videos matching applied filters`}</div>
           <div className='videos-container'>
             { 
               filteredVideos.map((video) => {
